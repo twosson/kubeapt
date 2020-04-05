8a666a451fcb89769571ed1e783b0a2661fca1b1
twosson <twosson@163.com>
2020-04-05 23:38:46 +0800

    Create API endpoints for overview

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Kubeapt
 
-Kubernetes apt
+Kubernetes dashboard for developers
+
+## Running 
+
+`$ apt dash`
+
+## Developing
+
+### Prerequisites
+
+* Go 1.14
+* npm
+* yarn
+
+### Environment variables
+
+* `DASH_DISABLE_OPEN_BROWSER`  - set to a non-empty value if you don't the browser launched when the dashboard start up.
+* `DASH_LISTENER_ADDR` - set to address you want dashboard service to start on. (e.g. `localhost:8080`)
+
+### Running development web UI
+
+`$ make setup-web`
+
+### Building binary with embedded web assets
+
+Create `./build/apt`: `$ make apt-dev`
---
+++ internal/api/api_test.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestAPI_routes(t *testing.T) {
+	cases := []struct {
+		path         string
+		expectedCode int
+	}{
+		{
+			path:         "/namespaces",
+			expectedCode: http.StatusOK,
+		},
+		{
+			path:         "/navigation",
+			expectedCode: http.StatusOK,
+		},
+		{
+			path:         "/content/",
+			expectedCode: http.StatusOK,
+		},
+		{
+			path:         "/content/nested",
+			expectedCode: http.StatusOK,
+		},
+		{
+			path:         "/missing",
+			expectedCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range cases {
+		name := fmt.Sprintf("GET: %s", tc.path)
+		t.Run(name, func(t *testing.T) {
+			srv := New("/")
+
+			ts := httptest.NewServer(srv)
+			defer ts.Close()
+
+			u, err := url.Parse(ts.URL)
+			require.NoError(t, err)
+
+			u.Path = tc.path
+
+			res, err := http.Get(u.String())
+			require.NoError(t, err)
+			defer res.Body.Close()
+
+			assert.Equal(t, tc.expectedCode, res.StatusCode)
+
+		})
+	}
+
+}
