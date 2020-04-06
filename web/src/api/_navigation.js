a3eb0e19de482faafb3a2e098845e394ffe38803
twosson <twosson@163.com>
2020-04-06 16:07:39 +0800

    adds beginnings of an api and moves nav data call into new structure

---
+++ web/src/api/_navigation.js
@@ -0,0 +1,17 @@
+const NavigationData = [
+  {
+    title: 'Overview',
+    path: '/overview'
+  },
+  {
+    title: 'Workloads',
+    path: '/workloads',
+    children: [
+      {
+        title: 'Cron Jobs',
+        path: '/cron-jobs'
+      }
+    ]
+  }
+]
+export default NavigationData
