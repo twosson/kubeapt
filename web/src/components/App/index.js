dbf7f1bceaf99dbfddc11d74f922774b94b0b2f9
twosson <twosson@163.com>
2020-04-11 16:18:04 +0800

    moves content generated state up into <App /> since it's derived from navigation & namespace (which live in App)

--- web/src/components/App/index.js
+++ web/src/components/App/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import Promise from 'promise'
 import _ from 'lodash'
-import { getNavigation, getNamespaces, getNamespace, setNamespace } from 'api'
+import { getNamespace, getNamespaces, getNavigation, setNamespace } from 'api'
 import Home from 'pages/Home'
 import Header from '../Header'
 import Navigation from '../Navigation'
@@ -12,8 +12,10 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      loading: false,
       navigation: [],
       namespaceOptions: [],
+      contents: [],
       namespaceOption: { label: 'default', value: 'default' }
     }
   }
@@ -22,11 +24,9 @@ class App extends Component {
     // Note(marlon): this logic for this should not live in <App />. it
     // might be better handled in a <Namespace /> container component or
     // in an HOC
-    const [navigation, namespacesPayload, namespacePayload] = await Promise.all([
-      getNavigation(),
-      getNamespaces(),
-      getNamespace()
-    ])
+    const [navigation, namespacesPayload, namespacePayload] = await Promise.all(
+      [getNavigation(), getNamespaces(), getNamespace()]
+    )
 
     let namespaceOptions = []
     if (
@@ -40,25 +40,60 @@ class App extends Component {
       }))
     }
 
-    let { namespaceOption } = this.state
+    let { contents, namespaceOption } = this.state
     if (namespacePayload && namespaceOptions.length) {
       const option = _.find(namespaceOptions, {
         value: namespacePayload.namespace
       })
-      if (option) namespaceOption = option
+      if (option) {
+        namespaceOption = option
+        contents = await this.fetchContents(namespaceOption.value)
+      }
     }
 
-    this.setState({ navigation, namespaceOption, namespaceOptions })
+    this.setState({
+      navigation,
+      namespaceOption,
+      namespaceOptions,
+      contents
+    })
+  }
+
+  async componentDidUpdate ({ location: { pathname: lastPath } }) {
+    const {
+      location: { pathname: thisPath }
+    } = this.props
+
+    if (thisPath && lastPath !== thisPath) {
+      this.setState({ contents: await this.fetchContents() })
+    }
+  }
+
+  fetchContents = async (namespace) => {
+    if (!namespace) namespace = this.state.namespaceOption.value
+    const {
+      location: { pathname }
+    } = this.props
+    const payload = await getContents(pathname, namespace)
+    if (payload) return payload.contents
+    return []
   }
 
   onNamespaceChange = async (namespaceOption) => {
+    this.setState({ namespaceOption, loading: true, contents: [] })
     const { value } = namespaceOption
     await setNamespace(value)
-    this.setState({ namespaceOption })
+    this.setState({ loading: false, contents: await this.fetchContents(value) })
   }
 
   render () {
-    const { navigation, namespaceOptions, namespaceOption } = this.state
+    const {
+      loading,
+      contents,
+      navigation,
+      namespaceOptions,
+      namespaceOption
+    } = this.state
     return (
       <div className='app'>
         <Header />
@@ -74,9 +109,9 @@ class App extends Component {
           <div className='app-main'>
             <Switch>
               <Route
-                path='/'
-                component={props => (
-                  <Home {...props} namespace={namespaceOption.value} />
+                path='/content/overview'
+                render={props => (
+                  <Home {...props} contents={contents} loading={loading} />
                 )}
               />
             </Switch>
