bdaca778a00c221cd2476d30e44a3425a61b5ad3
twosson <twosson@163.com>
2020-04-12 15:51:54 +0800

    adapts the UI to the new structure

--- web/src/components/Summary/components/Section/index.js
+++ web/src/components/Summary/components/Section/index.js
@@ -5,13 +5,16 @@ import CONFIG from './_config'
 import './styles.scss'
 
 export default function Section (props) {
-  const { title, data } = props
+  const { title, items } = props
   return (
     <div className='summary--component-section'>
       <div className='summary--component-title'>
         <h2>{title}</h2>
       </div>
-      {data.map(item => React.cloneElement(CONFIG.dataMap[item.type], { params: item }))}
+      {items.map((item, index) => React.cloneElement(CONFIG.dataMap[item.type], {
+        key: index,
+        params: item
+      }))}
     </div>
   )
 }
