6ad40393b3e994c4be5ac4c9f3e3705b92efa1b2
twosson <twosson@163.com>
2020-04-14 14:41:59 +0800

    use table empty content message when there are no rows

--- web/src/components/ContentTypes/shared/EmptyContent/index.js
+++ web/src/components/ContentTypes/shared/EmptyContent/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './styles.scss'
 
-export default function ({ title }) {
+export default function ({ emptyContent }) {
   return (
     <div className='content-empty'>
-      This namespace does not have any {title}.
+      {emptyContent}
     </div>
   )
 }
